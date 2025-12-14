791cef9c4d01028549f1a16a8713ac9aa7e5c56b
sygamminh-rgb <sygamminh@gmail.com>
2025-12-14 12:00:59 +0700

    Add project file

---
+++ MODEL/SinhVien.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlsv.QLSV.MODEL
+{
+
+    namespace MODEL
+    {
+        public class SinhVien
+        {
+            public string MaSV { get; set; }
+            public string HoTen { get; set; }
+            public DateTime NgaySinh { get; set; }
+            public string GioiTinh { get; set; }
+            public string MaLop { get; set; }
+        }
+    }
+    internal class sinhvien
+    {
+    }
+}
