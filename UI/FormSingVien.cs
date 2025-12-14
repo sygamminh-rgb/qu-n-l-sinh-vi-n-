588caf13731f5890c30d2abfc207b68dd59c4eb6
sygamminh-rgb <sygamminh@gmail.com>
2025-12-14 12:08:29 +0700

    Add project file

---
+++ UI/FormSingVien.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV1.UI
+{
+    public partial class FormSinhVien : Form
+    {
+        public FormSinhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormSinhVien_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
